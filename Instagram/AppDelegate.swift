5defe09b03716fd1c82126b571fd5e34d30641bf
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2018-08-11 00:55:22 -0700

    Persistent user

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -23,6 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://whispering-wave-72988.herokuapp.com/parse"
             })
         )
+      if PFUser.current() != nil {
+           let storyboard = UIStoryboard(name: "Main", bundle: nil)
+           //  view controller currently being set in Storyboard as default will be overridden
+           window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "HomeScreen")
+       }
 
         
         return true
